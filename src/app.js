ebf0e7c7b632b32bdd90c3351068da231866c64c
Yogesh Avadhut <you@example.com>
2026-01-14 12:02:43 +0530

    added curd apis

--- src/app.js
+++ src/app.js
@@ -18,10 +18,70 @@ app.post("/signup", async (req, res) => {
     }
     catch (err) {
         res.send({
-            message: "error come user are not created :(",
-            error: err.message
+            error: true,
+            message: ("error come user are not created :(", err.meassage)
         })
+    }
+})
 
+app.get('/user', async (req, res) => {
+
+    try {
+        const users = await UserModel.find({})
+        res.send(users)
+    }
+    catch (err) {
+        res.send({
+            error: true,
+            message: ("somthing went wrong :(", err.message)
+        })
+    }
+})
+
+app.patch("/user", async (req, res) => {
+    const data = req.body
+    const userId = req.body.userId
+
+    try {
+        const updateUser = await UserModel.findByIdAndUpdate(userId, data, { returnDocument: "before" })
+        console.log(updateUser)
+        res.send({
+            error: false,
+            message: "update successful :)",
+            from: updateUser,
+            to: data
+        })
+    }
+    catch (err) {
+        response.send({
+            error: true,
+            message: ("update unsuccessful :(", err.message)
+        })
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    userId = req.body.userId
+    try {
+        const deleteUser = await UserModel.findByIdAndDelete(userId)
+        if (deleteUser === null) {
+            res.send({
+                error: "false",
+                meassage: "record not deleted plese check userId",
+                recordId: userId
+            })
+        }
+        else {
+            res.send({
+                error: "false",
+                meassage: "record deleted successfully",
+                recordId: userId
+            })
+        }
+    }
+
+    catch (err) {
+        console.log("error comes :(", err)
     }
 })
 
